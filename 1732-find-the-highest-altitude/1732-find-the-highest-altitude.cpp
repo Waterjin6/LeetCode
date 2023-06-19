9c5c5f6be5431fa2a5fc9d09874b2957a3976229
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-06-19 16:25:28 +0900

    Time: 0 ms (100.00%), Space: 8.1 MB (20.89%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        vector<int> alti;
+        int max;
+        alti.push_back(0);
+        max = 0;
+        for(int i = 0; i < gain.size(); i++){
+            alti.push_back(alti[i] + gain[i]);
+        }
+        
+        for(int i = 0; i <= gain.size(); i++){
+            cout << alti[i] << " ";
+        }
+        
+        for(int i = 0; i < alti.size(); i++){
+            if(max < alti[i]) max = alti[i];
+        }
+        return max;
+    }
+};
