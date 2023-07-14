c2ef1923e72d61c01bcbee099ef68478a2b4e0b4
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-07-14 18:25:39 +0900

    Time: 7 ms (14.92%), Space: 8.6 MB (73.17%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    void printV(vector<int>& v){
-        cout << endl<< "vector : ";
-        for(int i =0; i < v.size(); i++){
-            cout <<v[i]<< " ";
-        }
-        return;
-    }
+
     vector<int> plusOne(vector<int>& digits) {
         bool isDone = false;
+        
         int i = digits.size()-1;
         
         while(isDone == false){
-            cout << digits[i]<<"d"<< endl;
             if(digits[i] < 9){ // 0~9
-                 cout <<"dddu"<< endl;
                 digits[i]++;
                 isDone = true;
             }
@@ -26,11 +19,9 @@ public:
                     isDone = true;
                 }
                 else{
-                    cout << "chu";
                     i--;
                 }
             }
-            printV(digits);
         }
         return digits;
     }
