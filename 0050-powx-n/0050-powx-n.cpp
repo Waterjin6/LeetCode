f2434abe2940e2e2b5388b88e7ea4893471788b0
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-07-25 08:54:40 +0900

    Time: 0 ms (100.00%), Space: 5.9 MB (92.19%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -5,15 +5,8 @@ public:
         if(n == 1)return x;
         if(n == -1)return 1/x;
         
-        if(n % 2 == 0){
-        return myPow(x*x, n/2);
-        }
-        else if(n > 0){   
-            return x*myPow(x*x, (n-1)/2);
-        }
-        else
-        {
-            return myPow(x*x, (n+1)/2)/x; 
-        }
+        if(n % 2 == 0)return myPow(x*x, n/2);
+        else if(n > 0)return x*myPow(x*x, (n-1)/2);
+        else return myPow(x*x, (n+1)/2)/x; 
     }
 };
