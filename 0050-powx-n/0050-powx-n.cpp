f10a16252c711889f6ec56268621272dbb9c8ae8
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-07-25 08:53:45 +0900

    Time: 0 ms (100.00%), Space: 6 MB (62.38%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if((n==0)||(x == 1))return 1;
+        if(n == 1)return x;
+        if(n == -1)return 1/x;
+        
+        if(n % 2 == 0){
+        return myPow(x*x, n/2);
+        }
+        else if(n > 0){   
+            return x*myPow(x*x, (n-1)/2);
+        }
+        else
+        {
+            return myPow(x*x, (n+1)/2)/x; 
+        }
+    }
+};
