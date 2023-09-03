4c5663ebb19664694d12ecd746b1f8afcf2d1909
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-09-04 08:45:27 +0900

    Time: 2 ms (44.35%), Space: 6.1 MB (71.41%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        if((m == 1)&&(n == 1))return 1;
+        if(m == 1)return 1;
+        if(n == 1)return 1;
+        
+        long long ans = 1;
+
+
+        int k = 2;
+        if(m < n){
+            int temp = m;
+            m = n;
+            n = temp;
+        }
+        
+        for(int i = n + m - 2; i > m-1; ans *= i, i--){
+            if(ans*i >= 2 * 1000000000){
+                ans /= k;
+                k++;
+            }
+        };
+        
+        for(; k <= n - 1; ans /= k, k++);
+
+        return (int)ans;
+    }
+};
