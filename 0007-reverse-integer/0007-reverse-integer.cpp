26447f63fcf532fca412b674dc64a81affc6a20a
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-10-22 18:00:29 +0900

    Time: 0 ms (100.00%), Space: 6.4 MB (11.76%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -3,11 +3,10 @@ public:
     int reverse(int x) {
 
         string ans= "";
-        if(x == -2147483648){
-            ans.append("-");
-            x = 2147483648;
-        }
-        else if(x < 0){
+        
+        if(x == -1*pow(2,31)) return 0;
+        
+        if(x < 0){
             ans.append("-");
             x *= -1;
         }
@@ -19,9 +18,10 @@ public:
         while(s[0] == '0'){
             s.erase(0,1);
         }
+        
         if(s.size() == 0)return 0;
         if(s.length() > 10)return 0;
-        if((s.length() == 10)&&(s > "2147483647"))return 0;
+        if((s.length() == 10)&&(s > to_string(pow(2,31)-1)))return 0;
         
         ans += s;
         
