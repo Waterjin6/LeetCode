97ff0d0c0e69d8b2aba7347e49ad5418cfff4b0f
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-10-25 22:51:58 +0900

    Time: 0 ms (100.00%), Space: 15.4 MB (43.50%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> ans;
+    
+    void makeString(int a, int b, string s){
+        if((a == 0)&&(b== 0)){
+            ans.push_back(s);
+            return;
+        }
+        
+        if(a > 0){
+            makeString(a-1, b, s + "(");
+        }
+        if((b > 0)&&(a < b)){
+            makeString(a, b-1, s+ ")");
+        }
+        
+        return;
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        makeString(n,n,"");
+        return ans;
+    }
+};
