2fd86b5f7c8113413a8ef73b0f481a53cee4a732
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-10-29 16:02:41 +0900

    Time: 4 ms (67.93%), Space: 7 MB (59.64%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void sayWord(string &ans){
+        string str = ""; 
+        char f = ans[0];
+        int cnt = 0;
+        
+        for(int i = 0; i < ans.length();){
+            while((i < ans.length()&&(f == ans[i]))){
+                cnt++;
+                i++;
+            }
+            str += to_string(cnt);
+            str += f;
+            cnt = 0;
+            if(i < ans.length())f = ans[i];
+        }
+        //cout<<"str : "<<str<<endl ;   
+        ans = str;
+        return;
+    }
+    
+    
+    string countAndSay(int n) {
+        string ans = "1";
+        if(n == 1)return ans;
+        
+        for(int i = 2; i <= n; i++){ // n times
+            sayWord(ans);
+        }
+        //cout<< ans<<endl;
+        return ans;
+    }
+};
