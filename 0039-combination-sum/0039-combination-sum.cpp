f6a4d0a363e48cd04b9d31259becd2c27603c009
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-10-29 16:59:52 +0900

    Time: 3 ms (85.01%), Space: 11.6 MB (44.94%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int target;
+    vector<vector<int>> ans;
+    vector<int> candidates;
+    
+    void work(int startIdx, vector<int> &vec, int sum){
+        if(sum == target){
+            ans.push_back(vec);
+            return;
+        }
+        
+        for(int i = startIdx; (i < candidates.size())&&(sum + candidates[i] <= target); i++){
+            vec.push_back(candidates[i]);
+            work(i,vec,candidates[i]+sum);
+            vec.pop_back();
+        }
+        
+        return;
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        this->target = target;    
+        sort(candidates.begin(), candidates.end());
+        this->candidates = candidates;
+        
+        for(int i = 0; i < candidates.size(); i++){
+            cout<<candidates[i]<<" ";
+        }
+        cout<<endl;
+        
+        for(int i = 0; (i < candidates.size()&&(candidates[i] <= target)); i++){
+            vector<int> v = {candidates[i]};
+            work(i, v ,candidates[i]);
+            //cout<<"done i : "<<i<<endl;
+            
+        }
+ 
+        return ans;
+    }
+};
