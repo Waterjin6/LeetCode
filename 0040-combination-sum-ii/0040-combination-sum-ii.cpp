45c09c8d920d1df3953b50ddaabb08fda041d7ae
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-10-30 17:12:02 +0900

    Time: 13 ms (14.13%), Space: 12.7 MB (19.91%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int target;
+    int cnt[51] = {0,};
+    
+    vector<vector<int>> ans;
+    
+    void work(int need, int startIdx, vector<int> vec){
+        
+        if(need == 0){
+            ans.push_back(vec);
+            return;
+        }
+        /*
+        for(int i; i < vec.size(); i++){
+            cout<< vec[i]<<" ";
+        }
+        cout<<endl;
+        */
+        
+        if(startIdx > need)return;
+        
+        while((startIdx <= target)&&(cnt[startIdx] == 0))startIdx++;
+        if(startIdx > target)return;
+            
+        int mx = (startIdx*cnt[startIdx] >= need) ? need/startIdx : cnt[startIdx];
+            
+        for(int t = 0; t <= mx; t++){
+            work(need - t*startIdx, startIdx+1, vec);
+            vec.push_back(startIdx);
+        }
+        
+        return;
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        this->target = target; 
+  
+        for(int i = 0; i < candidates.size(); i++){
+            cnt[candidates[i]]++;
+        }
+        
+        vector<int> v;
+        work(target, 1 , v);
+ 
+        return ans;
+    }
+};
