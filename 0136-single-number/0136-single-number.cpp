e7b9bb800ac4bd0a001ab8daad5d1e0581622030
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-01 15:41:57 +0900

    Time: 23 ms (21.29%), Space: 20.4 MB (10.96%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -3,9 +3,7 @@ public:
     int singleNumber(vector<int>& nums) {
         unordered_map<int,int> m;
         
-        for(int i = 0; i < nums.size(); i++){
-            m[nums[i]]++;
-        }
+        for(int i = 0; i < nums.size(); i++) m[nums[i]]++;
         
         for(auto i = m.begin(); i != m.end(); i++){
             if(i->second == 1)return i->first;
