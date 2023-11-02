2a1d2086be0c3bb86f1e4ded5aa38615699b34a3
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-02 16:17:34 +0900

    Time: 1184 ms (5.00%), Space: 497.2 MB (5.25%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+    vector<vector<string>> ans;
+    int dx[4] = {-1,-1,1,1};
+    int dy[4] = {-1,1,-1,1};
+    
+    bool inRange(int x, int y, int sz){
+        return x >= 0 && y >= 0 && x < sz && y < sz;
+    }
+    
+    bool isPossible(int x, int y, vector<string> v){
+        
+        for(int t = 0; t < 4; t++){
+            for(int i = 0; inRange(x+dx[t]*i, y+dy[t]*i, v.size()) == true; i++){
+                if(v[x+dx[t]*i][y+dy[t]*i]== 'Q')return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    void putQueen(int queenCnt, int x, int totalQ, vector<string> &v, bool isVisitedY[])
+    {
+        //cout<<"queen Cnt : "<<queenCnt<<endl;
+        /*for(int i = 0; i < totalQ; i++){
+            cout<< v[i]<<endl;
+        }
+        cout<<endl;*/
+        if(queenCnt == totalQ){
+            ans.push_back(v);
+            return;
+        }
+        if(x >= totalQ)return;
+        
+        for(; x < totalQ; x++){
+            //cout<<"x : "<<x<<endl;
+            for(int y = 0; y < totalQ; y++){
+                //cout<< x <<" "<< y << " "<<endl;
+                if((isVisitedY[y] == 0)&&(isPossible(x,y,v))){
+                    v[x][y] = 'Q';
+                    isVisitedY[y] = 1;
+                    putQueen(queenCnt+1,x+1,totalQ, v, isVisitedY);
+                    v[x][y] = '.';
+                    isVisitedY[y] = 0;
+                }
+            }
+        }
+        
+        return;
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        string s(n,'.');
+        vector<string> v(n,s);
+
+        /*for(int i = 0; i < n; i++){
+            cout<< v[i]<<endl;
+        }*/
+        bool visited[9] = {0,};
+        putQueen(0,0,n,v, visited);
+        
+        return ans;
+    }
+};
