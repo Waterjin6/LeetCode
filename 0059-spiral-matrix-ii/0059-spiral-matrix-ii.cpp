9e7fd1e983a1b8dd5efc84ef3bf8ba2a028c5e97
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-03 20:13:09 +0900

    Time: 3 ms (35.75%), Space: 7.2 MB (6.49%) - LeetHub

---
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int dx[4] = {0, 1, 0, -1}; //right, down, left, up
+    int dy[4] = {1, 0, -1, 0};
+    
+    void change(int &x, int &y, int &d, int &n, vector<vector<int>> &board){
+        if((x + dx[d] == -1)||(x + dx[d] == n)||(y + dy[d] == -1)||(y + dy[d] == n))d = (d+1)%4;
+        if(board[x + dx[d]][y + dy[d]] != 0)d = (d+1)%4;
+        
+        x += dx[d];
+        y += dy[d];
+        //cout<< x << " "<<y<<endl;
+        return;
+    }
+    
+    vector<vector<int>> generateMatrix(int n) {
+        vector<vector<int>> board(n,vector<int>(n,0));
+        
+        int x = 0, y = 0, d = 0, cw = 0, dw = n, ch = 0, dh = n-1;
+        
+        for(int i = 1; i <= n*n; i++){
+            board[x][y] = i;
+            if(n != 1)change(x,y,d,n, board);
+        }
+        
+        return board;
+    }
+};
