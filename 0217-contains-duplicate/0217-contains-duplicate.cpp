01ccb4dfad81a7ea72d614ea5c4b75509d70a5a1
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-08 16:37:36 +0900

    Time: 102 ms (49.63%), Space: 69.9 MB (61.23%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> m;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(m[nums[i]] > 0)return true;
+            m[nums[i]]++;
+        }
+        
+        return false;
+    }
+};
