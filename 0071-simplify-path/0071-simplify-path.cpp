f39a2665d8ce09dba22ab11c50c90715c3edb0d7
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-13 20:45:42 +0900

    Time: 8 ms (35.07%), Space: 7.6 MB (97.14%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.cpp
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -3,21 +3,16 @@ public:
     string simplifyPath(string path) {
         while(path.length() > 1 && path[0] != '/')path.erase(0,1);
         if(path[path.length()-1] != '/') path += '/';
+        
         stack<int> slash;
         slash.push(0);
         
         for(int i = 1; i < path.length();i++){
             if(path[i] == '/'){
-                if(slash.top() == i-1){
-                    //cout<< "a"<<endl;
-                    path.erase(i,1);
-                }
-                else if(slash.top() == i-2 && path[i-1] == '.'){
-                    //cout<<"y"<<endl;
-                    path.erase(i-1, 2);
-                }
+                if(slash.top() == i-1) path.erase(i,1);
+                else if(slash.top() == i-2 && path[i-1] == '.') path.erase(i-1, 2);
+            
                 else if(slash.top() == i-3 && path.substr(i-2, 2) == ".."){
-                    //cout<<"hey"<<endl;
                     path.erase(slash.top()+1,3);
                     i = slash.top();
                     if(slash.top() == 0)continue;
@@ -25,10 +20,8 @@ public:
                     slash.pop();
                     path.erase(slash.top()+1,i-slash.top());
                 }
-                else {
-                    //cout<< "B"<<endl;
-                    slash.push(i);
-                }
+                else slash.push(i);
+        
                 i = slash.top();
             }
         }
