4f27ec044f1e78101e4a973983bbee7fc19276e9
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-14 12:07:50 +0900

    Time: 5 ms (10.79%), Space: 6.3 MB (36.96%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isUgly(int n) {
+        int p[3] = {2,3,5};
+        while(n > 1){
+            bool f = false;
+            for(int i = 0; i < 3; i++){
+                if(n % p[i] == 0){
+                    n/= p[i];
+                    f = true;
+                }
+            }
+            if(f == false)return false;
+        }
+        if(n == 1)return true;
+        return false;
+    }
+};
