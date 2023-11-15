31ac2593338f740d6a3c04b17d2f1b38f6a4d444
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-15 17:36:00 +0900

    Time: 3 ms (62.25%), Space: 17 MB (20.51%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    
+    void getAns(int idx, vector<int>& unique, unordered_map<int,int> &cnt, vector<int> v){
+        //cout<< idx<<" "<< v.size()<<endl;
+        if(idx == unique.size()){
+            if(find(ans.begin(),ans.end(),v) == ans.end())ans.push_back(v);
+            return;
+        }
+        
+        getAns(idx+1, unique, cnt, v);
+        //cout<< unique[idx]<<" "<<cnt[idx]<<endl;
+        for(int i = 1; i <= cnt[unique[idx]]; i++){
+            v.push_back(unique[idx]);
+            getAns(idx+1, unique, cnt, v);
+        }
+        return;
+    }
+    
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<int> uniq, v;
+        unordered_map<int,int> cnt;
+    
+        for(int i = 0; i < nums.size(); i++){
+            if(cnt[nums[i]] == 0)uniq.push_back(nums[i]);
+            cnt[nums[i]]++;
+        }
+        getAns(0, uniq, cnt, v);
+        return ans;
+    }
+};
