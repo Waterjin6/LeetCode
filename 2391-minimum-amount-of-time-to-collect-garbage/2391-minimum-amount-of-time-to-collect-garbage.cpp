52e2fa928d1d1a7ae00569b0284e234a41a43b0a
sujin <64936396+Waterjin6@users.noreply.github.com>
2023-11-20 15:47:09 +0900

    Time: 218 ms (8.71%), Space: 102.4 MB (91.88%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int ans = 0;
+        char l[3] = {'G', 'P', 'M'};
+        
+        for(int i = 0; i < 3; i++){
+            char x = l[i];
+            bool f = false;
+            for(int r = garbage.size()-1; r >= 0; r--){
+                for(int t = 0; t < garbage[r].size();){
+                    if(x == garbage[r][t]){
+                        garbage[r].erase(garbage[r].begin()+t);
+                        ans++;
+                        f = true;
+                    }
+                    else t++;
+                }
+                if(r > 0 && f == true) {
+                    ans += travel[r-1];
+                    //cout<< "add "<< travel[r-1]<<endl;
+                }
+            }
+            cout<< ans<<endl;
+        }
+        return ans;
+    }
+};
