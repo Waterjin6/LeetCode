ecd58730550acb8a0417569dac3d76850d31b979
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-04 19:55:24 +0900

    Time: 7 ms (53.64%), Space: 7.7 MB (9.11%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char> ms;
+        unordered_map<char, char> mt;
+        
+        for(int i = 0; i < s.length(); i++){
+            if(ms.find(s[i]) == ms.end()){ // not registered
+                if(mt.find(t[i]) != mt.end())return false;
+                ms[s[i]] = t[i];
+                mt[t[i]] = s[i];
+            }
+            else if(ms[s[i]] != t[i])return false;
+        }
+        return true;
+    }
+};
