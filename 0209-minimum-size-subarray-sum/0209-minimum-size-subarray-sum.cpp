a7c8065f005453d6a6e7fb630fde509b5be83adf
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-04 19:48:14 +0900

    Time: 26 ms (80.17%), Space: 28.8 MB (14.85%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -6,19 +6,13 @@ public:
         int ns = nums.size();
         
         while(f < ns && b < ns){
-            //cout<<"1 : "<<b <<" "<< f <<endl;
-            while(b < ns-1 && sum < target){
-                sum += nums[++b];
-            }
-            //if(b == ns)break;
-
-            //cout<<"sum : "<<sum<< " "<< b <<" "<< f <<endl;
+            while(b < ns-1 && sum < target) sum += nums[++b];
             if(sum >= target)ans = min(ans, b-f+1);
-
             sum -= nums[f];
+            if(b == ns-1 && sum < target)break;
             f++;
         }
-        //cout<< sum<<endl;
+
         if(ans == INT_MAX)return 0;
         return ans;
     }
