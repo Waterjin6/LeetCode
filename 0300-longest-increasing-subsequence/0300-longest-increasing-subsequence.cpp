600a1706ac70e7fa6cb11518deaec1da783d211a
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-05 15:49:06 +0900

    Time: 247 ms (47.39%), Space: 10.8 MB (62.11%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -5,7 +5,7 @@ public:
         
         vector<int> cnt(ns, 0);
         
-        for(int i = ns-1; i >= 0; i--){
+        for(int i = ns-1; i >= 0 && ans < ns-i; i--){
             for(int t = i+1; t < ns; t++){
                 if(nums[i] < nums[t]){
                     cnt[i] = max(cnt[t]+1, cnt[i]);                    
