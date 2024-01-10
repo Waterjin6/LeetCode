a3809d5d2c1e8b8d7b9df46052c1cf11fedd08c4
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-10 21:59:52 +0900

    Time: 32 ms (17.62%), Space: 11.1 MB (5.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         vector<int> idxs;
-        int cnt = 0;
+        int cnt = 0, tl = t.length();
         map<char, int> n;
         string ans = s;
         
         for(int i = 0; i < s.length(); i++){
             if(find(t.begin(),t.end(), s[i]) != t.end())idxs.push_back(i);
         }
-        for(int i = 0; i < t.length(); i++)n[t[i]]++;
+        for(int i = 0; i < tl; i++)n[t[i]]++;
         
         map<char, int> c;
         int f = -1, b = 0;
         
         while(f <= b){
-            for(; b < idxs.size() && cnt < t.length(); b++){
+            for(; b < idxs.size() && cnt < tl; b++){
                 if(c[s[idxs[b]]] < n[s[idxs[b]]])cnt++;
                 c[s[idxs[b]]]++;
             }
-            if(cnt < t.length())break;
-            while(++f <= b && c[s[idxs[f]]] > n[s[idxs[f]]]){
-                c[s[idxs[f]]]--;
-            }
-            //cout<< cnt<<endl;
-            if(cnt == t.length() && ans.length() > idxs[b-1]-idxs[f]+1) ans = s.substr(idxs[f], idxs[b-1]-idxs[f]+1);
-            //cout<< s.substr(idxs[f], idxs[b-1]-idxs[f]+1) <<endl;
+            if(cnt < tl)break;
+            while(++f <= b && c[s[idxs[f]]] > n[s[idxs[f]]]) c[s[idxs[f]]]--;
+            
+            if(cnt == tl && ans.length() > idxs[b-1]-idxs[f]+1) ans = s.substr(idxs[f], idxs[b-1]-idxs[f]+1);
             c[s[idxs[f]]]--;
             cnt--;
-
-            //cout<< idxs[f]<<" "<<idxs[b-1]<<endl;
         }
-        //cout<< f<<" "<<b<<endl;
+
         if(f == -1)return "";
         return ans;
     }
