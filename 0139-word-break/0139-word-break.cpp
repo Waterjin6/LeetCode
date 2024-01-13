19d1c7afd1cc06d872281689eecb3a43f31101cb
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-13 20:25:41 +0900

    Time: 11 ms (49.23%), Space: 13.4 MB (43.40%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int sl = s.length();
+        vector<bool> dp(sl, false);
+        dp[0] =true;
+        
+        for(int i = 1; i <= sl; i++){
+            for(int j = i+1; j <= sl+1; j++){
+                if(dp[i-1]){
+                    //cout<< s.substr(i-1, j-i)<<endl;
+                    if(find(wordDict.begin(),wordDict.end(), s.substr(i-1, j-i)) != wordDict.end())dp[j-1] = true;
+                }
+            }
+        }
+        return dp[sl];
+    }
+};
