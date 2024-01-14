12064b1d73929d6150cac794957e1478cce4954f
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-01-14 15:09:20 +0900

    Time: 97 ms (69.67%), Space: 21 MB (78.40%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -9,23 +9,15 @@ public:
         
         if(ws != word2.length())return false;
         
+        for(int i = 0; i < ws; i++) m1[word1[i]]++;
+    
         for(int i = 0; i < ws; i++){
-            m1[word1[i]]++;
-        }
-        for(int i = 0; i < ws; i++){
-            if(m1[word2[i]] == 0){
-                //cout<< word2[i]<<endl;
-                return false;
-            }
+            if(m1[word2[i]] == 0) return false;
             m2[word2[i]]++;
         }
-        for(auto i = m1.begin(); i != m1.end(); i++){
-            v1.push_back(i->second);
-        }
         
-        for(auto i = m2.begin(); i != m2.end(); i++){
-            v2.push_back(i->second);
-        }
+        for(auto i = m1.begin(); i != m1.end(); i++) v1.push_back(i->second);
+        for(auto i = m2.begin(); i != m2.end(); i++) v2.push_back(i->second);
 
         sort(v1.begin(), v1.end());
         sort(v2.begin(), v2.end());
