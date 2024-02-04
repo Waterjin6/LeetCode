ac98418b18f77eb9b61a12720dd72a0c4b513a0f
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-02-04 15:37:55 +0900

    Time: 13 ms (83.41%), Space: 17.5 MB (46.21%) - LeetHub

---
+++ 0437-path-sum-iii/0437-path-sum-iii.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans = 0;
+    void getSum(TreeNode* root, long sum){
+        if(sum == root->val)ans++;
+        
+        if(root->left) {
+            getSum(root->left, sum-root->val);
+        }
+        if(root->right){
+            getSum(root->right, sum-root->val);
+        }
+        return;
+    }
+    
+    void getAns(TreeNode* root, int sum){
+        //cout<< root->val <<" "<<sum<<endl;
+        if(sum == root->val) {
+            //cout<< "ans : "<<root->val << " "<<sum<<endl;
+            ans++;
+        }
+        if(root->left) {
+            //cout<<"LEFT"<<endl;
+            getSum(root->left, sum-root->val);
+            getAns(root->left, sum);
+        }
+        if(root->right){
+            //cout<< "Right"<<endl;
+            getSum(root->right, sum-root->val);
+            getAns(root->right, sum);
+        }
+        return;
+    }
+    int pathSum(TreeNode* root, int targetSum) {
+        if(root) getAns(root, targetSum);
+        return ans;
+    }
+};
