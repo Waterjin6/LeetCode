72c15215a428d3dab60ec67f47f261bb21acdc65
sujin <64936396+Waterjin6@users.noreply.github.com>
2024-02-04 17:55:07 +0900

    Time: 174 ms (27.05%), Space: 83.3 MB (10.20%) - LeetHub

---
+++ 2462-total-cost-to-hire-k-workers/2462-total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        long long ans = 0; 
+        int f = 0, b = costs.size()-1, cs = costs.size();
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> front;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> back;
+        vector<bool> hired(cs, false);
+        
+        for(int i = 0; i < candidates; i++){
+            front.push({costs[f], f++});
+            back.push({costs[b], b--});
+        }
+        
+        while(k-- > 0){
+            //cout<< k <<" : ";
+            while(!front.empty() && hired[front.top().second]) front.pop();
+            while(!back.empty() && hired[back.top().second]) back.pop();
+            
+            while(front.size() < candidates && f < cs && hired[f] == false)front.push({costs[f],f++});
+            while(back.size() < candidates && b >= 0 && hired[b] == false)back.push({costs[b],b--});
+            if(k == 2)cout<< front.size()<<" "<< back.size()<<endl;
+            if(front.size() == 0 || back.top().first < front.top().first){
+                //cout<< back.top().first<<endl;
+                ans += back.top().first;
+                hired[back.top().second] = true;
+                back.pop();
+            }
+            else{
+                //cout<< front.top().first<<endl;
+                ans += front.top().first;
+                hired[front.top().second] = true;
+                front.pop();
+            }
+        }
+        
+        return ans;
+    }
+};
