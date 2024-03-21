6bf538b1b2d4ede8c9e9aeb0b45b43800d06e09e
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-03-21 20:52:25 +0900

    Time: 3 ms (98.19%), Space: 19 MB (17.40%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.cpp
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int  sz = nums.size();
-        vector<int> sum(10001, 0);
-        vector<int> skip(10001, 0);
+        vector<int> sum(10001, 0); // dp로 총합값 저장
         vector<int> get(10001, 0);
         
         for(int x : nums) sum[x] += x;
         
-        for(int i = 1; i < 10001; i++){
-            skip[i] = max(skip[i-1], get[i-1]);
-            get[i] = skip[i-1] + sum[i];
+        get[1] = sum[1];
+        
+        for(int i = 2; i < 10001; i++){
+            get[i] = max(get[i-1], get[i-2] + sum[i]);
         }
         
-        return max(skip[10000], get[10000]);
+        return get[10000];
     }
 };
