1b87eec2daa5358a711450a4a29f559d3cf8c122
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-03-22 14:32:47 +0900

    Time: 140 ms (51.61%), Space: 65.3 MB (18.19%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -2,40 +2,33 @@ class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         vector<int> ans;
-        int sz = graph.size(), qs, f;
+        int sz = graph.size(), f;
 
         vector<int> outCnt(sz, 0);
+        vector<bool> safe(sz, 0);
         vector<vector<int>> arrive_start(sz);
         queue<int> q;
         
         for(int i = 0; i < sz; i++){
+            for(int t = 0; t < graph[i].size(); t++) arrive_start[graph[i][t]].push_back(i);
             outCnt[i] = graph[i].size();
-            if(outCnt[i] == 0){
-                q.push(i);
-                ans.push_back(i);
-            }
-            else{
-                for(int t = 0; t < outCnt[i]; t++){
-                    arrive_start[graph[i][t]].push_back(i);
-                }
-            }
+            if(outCnt[i] == 0) q.push(i);
         }
+        //cout<< q.size()<<endl;
 
         while(!q.empty()){
-            qs = q.size();
-            for(int i = 0; i < qs; i++){
-                f = q.front();
-                q.pop();
-                for(int t = 0; t < arrive_start[f].size(); t++){
-                    if(--outCnt[arrive_start[f][t]] == 0){
-                        q.push(arrive_start[f][t]);
-                        ans.push_back(arrive_start[f][t]);
-                    }
-                }
+            f = q.front();
+            q.pop();
+            safe[f] = 1;
+            for(int t = 0; t < arrive_start[f].size(); t++){
+                if(--outCnt[arrive_start[f][t]] == 0) q.push(arrive_start[f][t]);
             }
         }
         
-        sort(ans.begin(),ans.end());
+        for(int i = 0; i < sz; i++){
+            if(safe[i] == 1)ans.push_back(i);
+        }
+        
         return ans;
     }
 };
