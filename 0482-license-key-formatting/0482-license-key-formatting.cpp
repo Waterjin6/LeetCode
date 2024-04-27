b3130a9b7cf4ebb3bb9196f35152dcb0df5e7d0c
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-04-27 21:45:20 +0900

    Time: 7 ms (71.44%), Space: 10 MB (25.17%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string licenseKeyFormatting(string s, int k) {
+        string ans = "", s1 = "";
+        string t = "";
+        int sl, fsz, idx = 0;
+        
+        for(char x : s){
+            if(x == '-') continue;
+            else s1 += (isdigit(x)) ? x : toupper(x);
+        }
+        
+        sl = s1.length();
+        if(sl == 0)return "";
+        fsz = (sl % k == 0 ) ? k : sl % k;
+        //cout<< sl <<" "<<fsz<<endl;
+        for(int i = 0; i < fsz; i++) ans += s1[idx+i];
+        idx += fsz;
+        
+        while(idx < sl){
+            ans += '-';
+            for(int i = 0; i < k; i++) ans += s1[idx+i];
+            idx += k;
+        }
+            
+        return ans;
+    }
+};
