184782baf1d939d96afbe4c516ce46c6cf43b920
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-07 21:26:31 +0900

    Time: 198 ms (65.32%), Space: 129.4 MB (36.39%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>> under(n);
+        
+        int ans = 0, xIdx,xSum, add, qs;
+        queue<pair<int,int>> q;
+        
+        for(int i = 0; i < n; i++) if(manager[i] > -1) under[manager[i]].push_back(i);
+        
+        q.push({headID, 0});
+        
+        while(!q.empty()){
+            xIdx = q.front().first;
+            xSum = q.front().second;
+            
+            q.pop();
+                
+            if(under[xIdx].size() == 0) ans = max(ans, xSum);
+            for(int i = 0; i < under[xIdx].size(); i++)q.push({under[xIdx][i], xSum + informTime[xIdx]});
+        }
+        return ans;
+    }
+};
