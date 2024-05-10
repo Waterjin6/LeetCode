b40a918a3fae54cad73436eae9e84ebeb1aeae7d
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-10 14:21:59 +0900

    Time: 261 ms (64.80%), Space: 64.6 MB (61.32%) - LeetHub

---
+++ 0786-k-th-smallest-prime-fraction/0786-k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        priority_queue<tuple<double,int,int>> pq;
+        int sz = arr.size();
+
+        for(int i = 0; i < sz; i++){
+            for(int t = sz-1; t >= 0; t--){
+                if(pq.size() < k)pq.push({double(arr[i])/double(arr[t]), arr[i], arr[t]});
+                else {
+                    if(get<0>(pq.top()) <= double(arr[i])/double(arr[t])) break;
+                    pq.pop();
+                    //cout<< "insert : "<< arr[i]<<" "<<arr[t]<<endl;
+                    pq.push({double(arr[i])/double(arr[t]), arr[i], arr[t]});
+                }
+            }
+            //cout<<get<1>(pq.top())<<" "<<get<2>(pq.top())<<endl;
+        }
+        //cout<<"sz : "<<pq.size()<<endl;
+        return {get<1>(pq.top()),get<2>(pq.top())};
+    }
+};
