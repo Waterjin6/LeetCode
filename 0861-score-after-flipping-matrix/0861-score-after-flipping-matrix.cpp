01ceba9df2894827170fcba968206a85f9c2a2bd
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-13 14:13:36 +0900

    Time: 0 ms (100.00%), Space: 10.2 MB (37.53%) - LeetHub

---
+++ 0861-score-after-flipping-matrix/0861-score-after-flipping-matrix.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) {
+        int ans = 0, xs = grid.size(), ys = grid[0].size(), cnt1 = 0, two = 0;
+        
+        // change row
+        for(int i = 0; i < xs; i++){
+            if(grid[i][0] == 0){
+                for(int t = 0; t < ys; t++) grid[i][t] = grid[i][t] ^ 1;
+            }
+        }
+        /*cout<<"print"<<endl;
+        for(int i = 0; i < xs; i++){
+            for(int t = 0; t < ys; t++)cout<< grid[i][t]<<" ";
+            cout<<endl;
+        }*/
+        // change col
+        for(int i = 0; i < ys; i++){
+            // calc cnt
+            cnt1 = 0;
+            for(int t = 0; t < xs; t++){
+                if(grid[t][i] == 1)cnt1++;
+            }
+            
+            if(2*cnt1 >= xs) continue;
+            
+            for(int t = 0; t < xs; t++){
+                grid[t][i] = grid[t][i] ^ 1;
+            }
+        }
+        //print 
+        /*cout<<"print"<<endl;
+        for(int i = 0; i < xs; i++){
+            for(int t = 0; t < ys; t++)cout<< grid[i][t]<<" ";
+            cout<<endl;
+        }
+        cout<<endl;*/
+        // calc ans
+        for(int i = ys-1; i >= 0; i--){
+            for(int t = 0; t < xs; t++) ans += grid[t][i] * pow(2, two);
+            two++;
+        }
+        return ans;
+    }
+};
