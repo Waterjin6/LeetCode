6df3633c40a8176b75884c23ef3b19789cd4ed49
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-18 20:23:52 +0900

    Time: 31 ms (42.81%), Space: 28.2 MB (63.87%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {   
+        int ans = duration, sz = timeSeries.size(), lastPoizoned = timeSeries[0]+duration-1;
+        
+        for(int i = 1; i < sz; i++){
+            if(lastPoizoned >= timeSeries[i]+duration-1){
+                //cout<< "pass "<<endl;
+                continue;
+            }
+            if(lastPoizoned >= timeSeries[i]){
+                ans += timeSeries[i]+duration-1 -lastPoizoned;
+                lastPoizoned = timeSeries[i] + duration -1;
+                //cout<<" 2 : "<< ans<<endl;
+                continue;
+            }
+            ans += duration;
+            lastPoizoned = timeSeries[i] + duration -1;
+            //cout<<ans<<endl;
+        }
+        
+        return ans;
+    }
+};
