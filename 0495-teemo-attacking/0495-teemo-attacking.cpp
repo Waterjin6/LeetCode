029e13738c05c34040146ea631ef4726431593b4
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-18 20:24:47 +0900

    Time: 25 ms (81.94%), Space: 28.2 MB (63.87%) - LeetHub

--- 0495-teemo-attacking/0495-teemo-attacking.cpp
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -4,19 +4,13 @@ public:
         int ans = duration, sz = timeSeries.size(), lastPoizoned = timeSeries[0]+duration-1;
         
         for(int i = 1; i < sz; i++){
-            /*if(lastPoizoned >= timeSeries[i]+duration-1){
-                //cout<< "pass "<<endl;
-                continue;
-            }*/
             if(lastPoizoned >= timeSeries[i]){
                 ans += timeSeries[i]+duration-1 -lastPoizoned;
                 lastPoizoned = timeSeries[i] + duration -1;
-                //cout<<" 2 : "<< ans<<endl;
                 continue;
             }
             ans += duration;
             lastPoizoned = timeSeries[i] + duration -1;
-            //cout<<ans<<endl;
         }
         
         return ans;
