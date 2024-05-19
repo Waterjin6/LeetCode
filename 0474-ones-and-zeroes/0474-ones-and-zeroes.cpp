f7d6cdb2667103e6b6578e736c5be68d2461c580
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-05-19 10:50:59 +0900

    Time: 167 ms (37.13%), Space: 149.6 MB (8.75%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -1,30 +1,34 @@
 class Solution {
 public:
-    void printMap(int &m, int &n, vector<vector<int>> &map){
-        cout<<"PRINT============"<<endl;
-        for(int i = 0; i <= m; i++){
-            for(int t = 0; t <= n; t++){
-                if(map[i][t] > 0) cout << "zero : "<<i <<" one : "<< t << " : "<< map[i][t]<<endl;
-            }
-        }
-        cout<<"================"<<endl<<endl;
-        return;
-    }
+
     void updateMap(int &z, int &o,int &m, int &n, vector<vector<int>> &map){
         vector<vector<int>> nMap(m+1, vector<int>(n+1, 0));
+        queue<tuple<int,int,int>> q;
+        int x, y, v;
         
-        //cout<< "z : "<< z<<" , o : "<<o <<endl;
-        for(int i = 0; i <= m; i++){
+        /*for(int i = 0; i <= m; i++){
             for(int t = 0; t <= n; t++){
                 if(map[i][t] > 0 && z+i <= m && o+t <= n) nMap[z+i][o+t] = map[i][t]+1;
             }
+        }*/
+        for(int i = 0; i <= m; i++){
+            for(int t = 0; t <= n; t++){
+                if(map[i][t] > 0 && z+i <= m && o+t <= n) q.push({z+i, o+t, map[i][t]+1});
+            }
+        }
+        
+        while(!q.empty()){
+            tie(x,y,v) = q.front();
+            q.pop();
+            
+            map[x][y] = max(map[x][y], v);
         }
 
-        for(int i = 0; i <= m; i++){
+        /*for(int i = 0; i <= m; i++){
             for(int t = 0; t <= n; t++){
                 if(nMap[i][t] > 0) map[i][t] = max(map[i][t],nMap[i][t]);
             }
-        }
+        }*/
         map[z][o] = max(map[z][o], 1);
         return;
     }
@@ -34,22 +38,17 @@ public:
         vector<vector<int>> map(m+1, vector<int>(n+1, 0)); 
         
         for(int i = 0; i < sz; i++){ // 수 맞추기
-            //cout<<"word : "<< strs[i]<<endl;
             z = 0;
             o = 0;
             for(char x : strs[i]){
                 if(x == '0')z++;
                 else o++;
             }
-            if(z <= m && o <= n){
-                updateMap(z,o,m,n,map);
-                //printMap(m, n, map);
-            }
+            if(z <= m && o <= n) updateMap(z,o,m,n,map);
         }
         
         for(int i = 0; i <= m; i++){
             for(int t = 0; t <= n; t++) {
-                //cout << "zero : "<<i <<" one : "<< t << " : "<< map[i][t]<<endl;
                 ans = max(ans, map[i][t]);
             }
         }
