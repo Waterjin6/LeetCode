93782c4a1cfe669070e1554aca533dbb6bb094b6
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2024-06-02 13:09:42 +0900

    Time: 0 ms (100.00%), Space: 7.4 MB (5.97%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.cpp
+++ 0507-perfect-number/0507-perfect-number.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
+        if(num == 1)return false;
         unordered_set<int> div;
         
-        for(int x = 1; x*x <= num; x++){
+        for(int x = 2; x*x <= num; x++){
             if(num % x == 0){
                 div.insert(num/x);
                 div.insert(x);
             }
         }
-        cout<< num<<endl;
-        num += num;
-        for(auto x : div){
-            num -= x;
-        }
-        //cout<< num<<endl;
-        if(num == 0)return true;
+
+        for(auto x : div) num -= x;
+        
+        if(num == 1)return true;
         return false;
     }
 };
