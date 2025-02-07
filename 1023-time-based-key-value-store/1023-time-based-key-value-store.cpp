14e765b29525acb54db6aee1f06a854c7e4e54f8
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2025-02-07 11:58:23 +0900

    Time: 80 ms (16.07%), Space: 138.2 MB (36.16%) - LeetHub

---
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.cpp
@@ -0,0 +1,31 @@
+class TimeMap {
+public:
+    map<string, map<int, string>*> mapP;
+
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        // key가 없으면 새로운 map을 동적 할당
+        if (mapP.find(key) == mapP.end()) mapP[key] = new map<int, string>();
+    
+        // 포인터를 통해 insert() 호출
+        mapP[key]->insert({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (mapP.find(key) == mapP.end()) return "";
+
+        auto it = mapP[key]->upper_bound(timestamp);
+        if (it == mapP[key]->begin()) return ""; // timestamp 이하 값이 없음
+        --it;
+        return it->second;
+    }
+};
+
+/**
+ * Your TimeMap object will be instantiated and called as such:
+ * TimeMap* obj = new TimeMap();
+ * obj->set(key,value,timestamp);
+ * string param_2 = obj->get(key,timestamp);
+ */
