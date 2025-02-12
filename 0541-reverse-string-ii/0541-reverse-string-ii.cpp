a689297975ed89ebd595f63abd8e47d1016d92f2
Sujin Kang <64936396+Waterjin6@users.noreply.github.com>
2025-02-12 16:20:12 +0900

    Time: 0 ms (100%), Space: 9.5 MB (45.9%) - LeetHub

---
+++ 0541-reverse-string-ii/0541-reverse-string-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+        int sz = s.length(), idx = 0;
+
+        while(idx + 2*k <= sz){
+            reverse(s.begin()+idx, s.begin()+idx+k);
+            idx += 2*k;
+        }
+
+        if(idx + k <= sz){
+            reverse(s.begin()+idx, s.begin()+idx+k);
+        }
+        else if(idx+k > sz){
+            reverse(s.begin()+idx, s.end());
+        }
+        return s;
+    }
+};
